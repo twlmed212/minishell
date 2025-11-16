4bf5782b78c5aebf25cd0829eaadf317be22c85c
Mtawil <twlmed212@gmail.com>
2025-11-17 00:13:20 +0100

    add Builtins detection : export, unset, cd

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtawil <mtawil@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/16 02:45:49 by mtawil            #+#    #+#             */
+/*   Updated: 2025/11/16 02:45:50 by mtawil           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/minishell.h"
 
 int is_builtin(char *cmd)
@@ -22,7 +34,7 @@ int is_builtin(char *cmd)
     return (0);
 }
 
-int execute_builtin(char **args, char **env)
+int execute_builtin(char **args, t_shell *shell)
 {
     int len = ft_strlen(args[0]);
 
@@ -39,7 +51,13 @@ int execute_builtin(char **args, char **env)
     if (len == 4 && ft_strncmp(args[0], "exit", 4) == 0)
         return (builtin_exit(args));
     if (len == 3 && ft_strncmp(args[0], "env", 3) == 0)
-        return (builtin_env(env));
+        return (builtin_env(args, shell));
+    if (len == 2 && ft_strncmp(args[0], "cd", 2) == 0)
+        return (builtin_cd(args));
+    if (ft_strncmp(args[0], "export", 6) == 0)
+        return (builtin_export(args, shell));
+    if (ft_strncmp(args[0], "unset", 5) == 0)
+            return (builtin_unset(args, shell));
     
     printf("minishell> %s: command not found\n", args[0]);
     return (0);
